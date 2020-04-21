ad3bfe00c2ab25ea8d3abed7aeff7c43d2943995
Shanu1515 <sm6838@srmist.edu.in>
2020-04-22 00:51:08 +0530

    new commit

---
+++ src/portfolio.js
@@ -0,0 +1,203 @@
+/* Change this file to get your personal Porfolio */
+
+const greeting = {
+  /* Your Summary And Greeting Section */
+  title: "Hi all I'm Shanu Mishra",
+  subTitle:
+    "🚀A passionate Computer Science Student having an experience of building Mobile applications with  React Native and some other cool libraries and frameworks.Also having interest in Programming,Data Analytics,Blockchain and Digital Marketing",
+  resumeLink: "https://drive.google.com/file/d/1XX1d8_Frn3a0ol4YUKGDCG_JzvW8cB8Q/view"
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/Shanu1515",
+  linkedin: "https://www.linkedin.com/in/shanu-mishra/",
+  gmail: "mishra.shanu15@gmail.com",
+ 
+  facebook: "https://www.facebook.com/profile.php?id=100005616668098"
+};
+
+const skillsSection = {
+  /* Your Skills Section  */
+  title: "What i do",
+  subTitle: " FRONT-END DEVELOPER WHO LIKES TO EXPLORE VARIOUS TECH STACKS",
+  skills: [
+    "⚡TECHNICAL:Web/App developer,competitive coding,data analyst,digital marketing",
+    
+    "⚡ PERSONAL:Leadership,Management, Adaptable,Interpersonal skills",
+    
+    
+  ],
+
+  /* Make Sure You include correct Font Awesome Classname to view your icon */
+  /* https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+   
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    
+    {
+      skillName: "Android",
+      fontAwesomeClassname: "fab fa-android"
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "R-programming",
+      fontAwesomeClassname: "fab fa-r-project"
+    },
+    {
+      skillName: "blockchain",
+      fontAwesomeClassname: "fab fa-ethereum"
+    },
+    
+  ]
+};
+
+const openSource = {
+  /* Your Open Source Section to View Your Github Pinned Projects */
+  /* To know how to get github key look at readme.md */
+
+  githubConvertedToken: "",
+  githubUserName: ""
+};
+
+const bigProjects = {
+  /* Some Big Projects You have worked with your company */
+  title: "Ongoing Projects💻",
+  subtitle: "",
+  projects: [
+    {
+      
+      image: "https://s3images.coroflot.com/user_files/individual_files/535932_qzur9pqrerbkduhyeiddfzxuf.png",
+      link: ""
+    },
+    {
+      image: "https://media-exp1.licdn.com/dms/image/C510BAQFYMlOtl5TPzQ/company-logo_200_200/0?e=2159024400&v=beta&t=TSswXyZGTljueqdVC4nvOE0qVemaozzqQWwzc5aHlCQ",
+      link: ""
+    }
+  ]
+};
+
+const achievementSection = {
+  /* Your Achievement Section Include Your Certification Talks and More */
+
+  title: "Achievements🏆 ",
+  
+
+  achivementsCards: [
+    {
+      title: "Silver Elite Certiﬁcate in NPTEL for the course Joy Of Computing Using Python",
+      image:"https://i.ytimg.com/vi/it8fdS5Axfo/maxresdefault.jpg",
+      
+      footerLink: [
+        { name: "Certification", url: "https://nptel.ac.in/noc/Ecertificate/?q=noc19-cs09/NPTEL19CS09S61570110191151331.jpg" },
+        
+      ]
+    },
+    {
+      title: "Selected in top 10 of national level Hackathon, Hackbout 2020",
+      
+      image: "https://d8it4huxumps7.cloudfront.net/uploads/images/opportunity/gallery/5e4401db3091a_e040887e-35cf-4c3c-a5c5-e46ed6dd507a.jpeg?d=1200x700",
+      footerLink: [{ name: "Awards", url: "https://drive.google.com/file/d/1lro8ThHKlZXAMj5ifSoSF93mPoiJdREE/view?usp=sharing" }]
+    },
+
+    {
+      title: "Student of the Year award for overall excellence ",
+      
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQNU-2XWhPh7TjrBee3oCr-WVC2LPJglp3kcnJSZWlRyLHbXFti&usqp=CAU",
+      footerLink: [
+        { name: "Certification", url: "" },
+       
+      ]
+    }
+  ]
+};
+
+const blogSection = {
+  /* Blogs Section */
+
+  title: "Blogs📝",
+  subtitle: "",
+
+  blogs: [
+    {
+      url: "https://tipstricksnhacks.wordpress.com/",
+      image: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201909/tips-and-tricks-647_0-770x433.jpeg?iivJ1BRlo8.tHTkRFV3KMK3t6nNCWDjD",
+      title: "Hacks4You",
+      description: "Do you want to know some mind blowing tricks and hacks which will make your everyday tasks more easier."
+    },
+    {
+      url: "",
+      image: "https://image.shutterstock.com/image-illustration/coming-soon-neon-sign-purple-260nw-1454233298.jpg",
+      title: "",
+      description: "Something really interesting coming soon"
+    }
+  ]
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "Courses and certifications",
+  subtitle: "",
+
+  talks: [
+    {
+      title: "Check out the courses here👇",
+      subtitle: "",
+      slides_url: "https://drive.google.com/drive/folders/1X1yuJUx4qRZnlZ-Mht5Hb5JR0Ip1BZmn?usp=sharing",
+      event_url: "https://drive.google.com/drive/folders/1X1yuJUx4qRZnlZ-Mht5Hb5JR0Ip1BZmn?usp=sharing",
+      image:
+        ""
+    }
+  ]
+};
+
+// Podcast Section
+const podcastSection = {
+  title: "",
+  subtitle: "",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [""]
+};
+
+const contactInfo = {
+  title: "Contact Me 📞",
+  subtitle: "Discuss a project or just want to say hi,my inbox is open for all",
+  number: "9971715927",
+  email_address: "mishra.shanu15@gmail.com"
+};
+
+export { greeting, socialMediaLinks, skillsSection,openSource, bigProjects, achievementSection, blogSection, talkSection, podcastSection, contactInfo };
